20ab0f28d94b82e6178bb47ed8b56f16055d6a2c
Яша Тюттерин <asatutterin@Asas-MacBook-Air.local>
2021-12-11 17:53:04 +0300

    Junit add project

--- city-register/src/main/java/gitsTest.java
+++ city-register/src/main/java/gitsTest.java
@@ -1,6 +1,19 @@
+import java.util.LinkedList;
+import java.util.List;
 
 public class gitsTest {
 	public static void main(String[] args) {
 		System.out.println("Hello GITHUB");
-	}
+		
+		
+		List<Long> list = new LinkedList<Long>();
+		for(Long i=0L; i<20; i++) {
+			list.add(i);
+			
+		}
+		for(Long l: list) {
+			System.out.print(l+" ");
+		}
+		
+ 	}
 }
